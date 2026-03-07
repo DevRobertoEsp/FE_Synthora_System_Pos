92c9ac67f0729d4f5f15c47a4d896b0cb06b1268
GitXhap <roberto.espinoza.n@vallegrande.edu.pe>
2026-03-07 15:31:03 -0500

    Axios Nube

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
  // baseURL: 'http://localhost:8085', // Spring Boot LocalHost
 })
 
