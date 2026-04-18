29fe3cec6155abb21ccbb43ac244d1c23d037b70
IngXhap <roberto.espinoza.n@vallegrande.edu.pe>
2026-04-18 11:14:56 -0500

    Change port

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'https://be-synthora-system-pos-xpmc.onrender.com', // Spring Boot LocalHost
+  baseURL: import.meta.env.VITE_API_URL,
+  //baseURL: 'https://be-synthora-system-pos-xpmc.onrender.com', // Spring Boot LocalHost
   //baseURL: 'http://localhost:8085',
 })
 
