2a812978b85d5a3102e24aaf29e964d79230f6ee
IngXhap <roberto.espinoza.n@vallegrande.edu.pe>
2026-04-18 10:50:48 -0500

    Cloud

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'http://localhost:8085', // Spring Boot LocalHost
+  baseURL: 'https://be-synthora-system-pos-xpmc.onrender.com', // Spring Boot LocalHost
+  //baseURL: 'http://localhost:8085',
 })
 
 // Interceptor — agrega el token automáticamente a cada request
